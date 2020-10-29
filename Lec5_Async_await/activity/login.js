8663cf92bd186b49498338be633a1b22f2186036
sushberiwal <sushantberiwal@gmail.com>
2020-10-29 17:04:23 +0530

    addChallenges complete

--- Lec5_Async_await/activity/login.js
+++ Lec5_Async_await/activity/login.js
@@ -2,6 +2,8 @@ const puppeteer = require("puppeteer");
 const challenges = require("./challenges");
 // [  {} , {} , {} , {} ,{}  ]
 
+let tab;
+
 (async function () {
   try {
     let browser = await puppeteer.launch({
@@ -11,6 +13,7 @@ const challenges = require("./challenges");
     })
     let pages = await browser.pages();
     let page = pages[0];
+    tab = page;
     await page.goto("https://www.hackerrank.com/auth/login");
     await page.type("#input-1", "tifet30346@gmajs.net");
     await page.type("#input-2", "12345678");    
@@ -22,17 +25,57 @@ const challenges = require("./challenges");
     let bothLis = await page.$$(".nav-tabs.nav.admin-tabbed-nav li");
     let manageChallenge = bothLis[bothLis.length-1];
     // <li>  </li>   
+    // manage challenge page
     await Promise.all([ page.waitForNavigation({waitUntil:"networkidle0"})   , manageChallenge.click() ]);
+    let url = await page.url();
+    
+    // create challenge page
     await Promise.all([ page.waitForNavigation({waitUntil:"networkidle0"})   , page.click(".btn.btn-green.backbone.pull-right")]);
     // pending promise to create one challenge
-    // await createChallenge(challenges[0]);
+    await createChallenge(challenges[0]);
+
+    for(let i=1 ; i<challenges.length ; i++){
+        await tab.goto(url);
+        await Promise.all([ page.waitForNavigation({waitUntil:"networkidle0"})   , page.click(".btn.btn-green.backbone.pull-right")]);
+        await createChallenge(challenges[i]);
+    }
+    await tab.goto(url);
   } 
   catch (error) {
 
   }
 })();
+// challenge = {
+//     "Challenge Name": "Pep_Java_1GettingStarted_1IsPrime",
+//     "Description": "Question 1",
+//     "Problem Statement": "Take as input a number n. Determine whether it is prime or not. If it is prime, print 'Prime' otherwise print 'Not Prime.",
+//     "Input Format": "Integer",
+//     "Constraints": "n <= 10 ^ 9",
+//     "Output Format": "String",
+//     "Tags": "Basics"
+//   }
 
-
-// async function createChallenge(challenge){
-
-// }
+async function createChallenge(challenge){
+    try{
+        let name = challenge["Challenge Name"];
+        let description = challenge["Description"];
+        let problem = challenge["Problem Statement"];
+        let input = challenge["Input Format"];
+        let constraints = challenge["Constraints"];
+        let output = challenge["Output Format"];
+        let tags = challenge["Tags"];
+        await tab.waitForSelector("#name" , {visible:true});
+        await tab.type("#name" , name );
+        await tab.type("#preview" , description );
+        await tab.type("#problem_statement-container .CodeMirror textarea" , problem);
+        await tab.type("#input_format-container .CodeMirror textarea" , input);
+        await tab.type("#constraints-container .CodeMirror textarea"   , constraints);
+        await tab.type("#output_format-container .CodeMirror textarea" , output);
+        await tab.type("#tags_tag" , tags);
+        await tab.keyboard.press("Enter");
+        await tab.click(".save-challenge.btn.btn-green");
+    }
+    catch(error){
+        return error;
+    }
+}
